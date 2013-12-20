70e179df79d9ba5633c9b977cece5e90e11d9d04
mustafaeyupoglu <mustafa.eyupoglu95@gmail.com>
2013-12-20 12:37:47 +0200

    Create
    Proje oluşturuldu

---
+++ SqlConnectionAçma/ConnectionClass.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SqlConnectionAçma
+{
+    class ConnectionClass
+    {
+        public string server = @"MUSTAFA1\SQLEXPRESS";
+        public string user = "admin";
+        public string sifre = "admin";
+        public string database = "Ak_Win";
+        public SqlConnection BaglantiAcma()
+        {
+            SqlConnection baglanti = new SqlConnection();
+
+            baglanti.ConnectionString = @"server=" + server + "; user=" + user + "; pwd=" + sifre + "; database=" + database + "";
+
+            baglanti.Open();
+
+            return baglanti;
+
+
+
+
+        }
+    }
+}
